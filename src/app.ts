e0da50c81063f9eea03c744155789e40f37e9f02
fred <fred.gauthier.dev@gmail.com>
2021-11-09 10:46:47 +0100

    Fix CORS issue

--- src/app.ts
+++ src/app.ts
@@ -8,11 +8,17 @@ import "reflect-metadata";
 import morgan from "morgan";
 import { access } from "./config/logger";
 import { errorHandler } from "./middlewares/errors";
+import cors from "cors"
 
 dotenv.config();
 const app = express();
 // if you run behind a proxy (e.g. nginx)
 // app.set("trust proxy", 1);
+
+app.use(cors({
+  // origin: true,
+  credentials: true,
+}))
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -32,6 +38,11 @@ process.env.NODE_ENV === "development"
     );
 
 //Configure the Session middleware
+declare module 'express-session' {
+  export interface SessionData {
+    isAuth: boolean;
+  }
+}
 app.use(session);
 
 const router = new Router();
